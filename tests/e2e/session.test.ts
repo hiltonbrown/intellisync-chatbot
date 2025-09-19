fbe7587539bc8d95748f40ea1c0155cbbf0df781
Hilton Brown <hello@hiltonbrown.com.au>
2025-09-19 18:28:03 +1000

    general fixes

--- tests/e2e/session.test.ts
+++ tests/e2e/session.test.ts
@@ -3,6 +3,7 @@ import { AuthPage } from '../pages/auth';
 import { generateRandomTestUser } from '../helpers';
 import { ChatPage } from '../pages/chat';
 import { getMessageByErrorCode } from '@/lib/errors';
+import type { Request } from '@playwright/test';
 
 test.describe
   .serial('Guest Session', () => {
@@ -15,7 +16,7 @@ test.describe
         throw new Error('Failed to load page');
       }
 
-      let request = response.request();
+      let request: Request | null = response.request();
 
       const chain = [];
 
@@ -57,7 +58,7 @@ test.describe
         throw new Error('Failed to load page');
       }
 
-      let request = response.request();
+      let request: Request | null = response.request();
 
       const chain = [];
 
